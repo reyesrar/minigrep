192c38d40990329ba18c2d933250d466bd9608e9
reyesrra <reyesrainny11@gmail.com>
2024-06-15 22:38:00 -0400

    feat: agregada impresion de numeros y lineas de coincidencia

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<algorithm>
 using namespace std;
 
 int main(int argc, char* argv[]){
@@ -21,17 +22,23 @@ int main(int argc, char* argv[]){
 
     string line;
     int lineNumber = 1;
+    int linesMatches = 0;
+    int matches = 0;
 
     cout<<"\n"<<endl;
     while (getline(file, line)) {
         if (line.find(searchString) != string::npos) {
             cout << "Linea " << lineNumber << ": " << line << endl;
+            linesMatches++;
+            matches += std::count(line.begin(), line.end(), searchString[0]);
         }
         lineNumber++;
-
     }
-    cout<<"\n"<<endl;
     file.close();
+
+    cout << "Numero de lineas con coincidencias: " << linesMatches << endl;
+    cout << "Numero de coincidencias: " << matches << endl;
+    cout<<"\n"<<endl;
 
     return 0;
         
