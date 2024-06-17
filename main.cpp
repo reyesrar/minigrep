e9947496e9884e263237eff4c8cbde85b371b902
reyesrra <reyesrainny11@gmail.com>
2024-06-17 13:33:57 -0400

    feat: impresion de coincidencias a buscar en color

--- main.cpp
+++ main.cpp
@@ -4,8 +4,13 @@
 #include<algorithm>
 using namespace std;
 
+void matchColor(int color){
+        cout<<"\033["<<color<<"m";
+    }
+
 int main(int argc, char* argv[]){
 
+    
     if (argc != 3) {
         cerr << "Uso: " << argv[0] << " <archivo> <cadena de busqueda>" << endl;
         return 1;
@@ -14,7 +19,7 @@ int main(int argc, char* argv[]){
     string filename = argv[1];
     string searchString = argv[2];
 
-     transform(searchString.begin(), searchString.end(), searchString.begin(), ::tolower); // Convertir la cadena de búsqueda a minúsculas
+    transform(searchString.begin(), searchString.end(), searchString.begin(), ::tolower); // Convertir la cadena de búsqueda a minúsculas
 
     ifstream file(filename);
     if (!file.is_open()) {
@@ -31,8 +36,15 @@ int main(int argc, char* argv[]){
     while (getline(file, line)) {
         string temp = line;
         transform(line.begin(), line.end(), line.begin(), ::tolower);
-        if (line.find(searchString) != string::npos) {
-            cout << "Linea " << lineNumber << ": " << temp << endl;
+        size_t found = line.find(searchString);
+        if (found != string::npos) {
+            cout << "Linea " << lineNumber << ": ";
+            cout<<temp.substr(0,found);
+            matchColor(32);
+            cout<<temp.substr(found, searchString.size());
+            matchColor(0);
+            cout<<temp.substr(found + searchString.size());
+            cout<<"\n"<<endl;
             linesMatches++;
             matches += count(line.begin(), line.end(), searchString[0]);
         }
@@ -40,6 +52,7 @@ int main(int argc, char* argv[]){
     }
     file.close();
 
+    cout<<"\n"<<endl;
     cout << "Numero de lineas con coincidencias: " << linesMatches << endl;
     cout << "Numero de coincidencias: " << matches << endl;
     cout<<"\n"<<endl;
